097e553524b618bee38ef607fc20693d2bfe4785
muhammedashikCQ <muhammed.ashik@controlqore.com>
2023-07-11 12:27:06 +0530

    csharp added

---
+++ Csharp Assignments/Csharp Assignments 3 - July 10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Linq;
+
+public class EmployeeManagement
+{
+
+    public int EmployeeID;
+    public string EmployeeName;
+    public double EmployeeSalary;
+    public EmployeeManagement(int id)
+    {
+        EmployeeID = id;
+    }
+    public EmployeeManagement(int id, string name) : this(id)
+
+    {
+        EmployeeName = name;
+    }
+    public EmployeeManagement(int id, string name, double salary) : this(id, name)
+    {
+        EmployeeSalary = salary;
+    }
+    public void display()
+    {
+        Console.WriteLine("Employee Details :");
+        Console.WriteLine("Employee Id : " + EmployeeID);
+        Console.WriteLine("Employee Name :" + EmployeeName);
+        Console.WriteLine("Employee Salary :" + EmployeeSalary);
+    }
+    static void Main(string[] args)
+    {
+        EmployeeManagement employee = new EmployeeManagement(10, "ashik", 250000);
+        employee.display();
+
+    }
+
+}
+
+
