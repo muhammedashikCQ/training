9551b1195bddde4501cc6fcbc8b0de8cc3d42dd5
muhammedashikCQ <muhammed.ashik@controlqore.com>
2023-07-26 18:57:06 +0530

    Csharp updated

---
+++ Csharp Assignments/CsharpAssignment8-July19/Employee.Model/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace Employee.Model
+{
+    public class Employee
+    {
+        public static int Id { get; set; }
+        public static string Name { get; set; }
+        public static int Salary { get; set; }
+        static Employee()
+        {
+            Id = 1;
+            Name = "ashik";
+            Salary = 1000;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Employee.Id);
+        }
+    }
+}   
+
