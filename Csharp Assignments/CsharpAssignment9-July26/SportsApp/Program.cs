4fa2f42ff68edb7631905cc7e48e87a40f29e304
muhammedashikCQ <muhammed.ashik@controlqore.com>
2023-07-27 11:00:10 +0530

    Csharp updated

---
+++ Csharp Assignments/CsharpAssignment9-July26/SportsApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using SportsApp;
+public class SpotsApp
+{
+    static void Main(string[] args)
+    {
+        Sports object1 = new Sports('l');
+    }
+}
---
+++ Csharp Assignments/CsharpAssignment9-July26/SportsApp/Sports.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsApp
+{
+    public class Sports
+    {
+        public char input { get; set; }
+        public Sports(char Charecter) 
+        {
+            input=Charecter;
+
+            try
+            {
+                if(input =='c')
+                {
+                    Console.WriteLine("Cricket");
+                }
+                else if(input =='f')
+                {
+                    Console.WriteLine("Football");
+
+                }
+                else if (input =='h') 
+                {
+                    Console.WriteLine("Hockey");
+                }
+                else if(input=='t')
+                {
+                    Console.WriteLine("Tennis");
+                }
+                else if(input =='b')
+                {
+                    Console.WriteLine("Badminton");
+                }
+                else
+                {
+                   throw new InvalidUserInputException("Invalid Input");
+                }
+            }
+            catch(InvalidUserInputException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+    public class InvalidUserInputException : Exception
+    {
+        public InvalidUserInputException(string message):base(message) 
+        { 
+
+        }
+     
+    }
+}
