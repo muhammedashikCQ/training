12a9afb428449885dd42653115f764730f1ee8a0
muhammedashikCQ <muhammed.ashik@controlqore.com>
2023-07-27 10:19:48 +0530

    Csharp updated

---
+++ Csharp Assignments/CsharpAssignment8-July26/Employee.Model/Employee.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeModel
+{
+    public class Employee
+    {
+        public static int Count { get; set; }
+        private string Id = "Emp1000";
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string EmployeeType { get; set; }
+        static Employee()
+        {
+            Count = 0;
+        }
+        public Employee(string name, int salary)
+        {
+            name = Name;
+            salary = Salary;
+            Count += 1;
+            int x = int.Parse(Id.Substring(3));
+            x += 1;
+            Id = "Emp" + x;
+        }
+        public void Display()
+        {
+            Console.WriteLine("The Total Number Of Employee is :  " + Employee.Count);
+        }
+
+    }
+}
---
+++ Csharp Assignments/CsharpAssignment8-July26/EmployeeApp/EmployeeApp.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeApp;
+using EmployeeModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    public class EmployeeApp
+    {
+
+        static void Main(string[] args)
+        {
+            Employee object1 = new Employee("John doe", 15000);
+            Employee object2 = new Employee("Liam Smith", 20000);
+            Employee object3 = new Employee("Mary James", 15000);
+            object3.Display();
+        }
+
+    }
+}
