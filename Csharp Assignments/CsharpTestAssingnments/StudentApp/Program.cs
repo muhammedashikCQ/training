dea8e2d23a0148a36456e54ed901dec4b70190cb
muhammedashikCQ <muhammed.ashik@controlqore.com>
2023-07-28 11:48:49 +0530

    Csharp updated

---
+++ Csharp Assignments/CsharpTestAssingnments/StudentApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using StudentApp;
+
+public class program
+{
+    static void Main(string[] args)
+    {
+        List<Student> stdlist = new List<Student>
+        {
+            new Student(1, "John Doe", 85),
+            new Student(2, "Liam Smith", 70),
+            new Student(3, "Mary James", 62),
+            new Student(4, "Davide James", 55),
+            new Student(5, "Lionel Messi", 100),
+            new Student(6, "Ronaldo", 90),
+            new Student(7, "Neymar", 85)
+        };
+        var z=stdlist.ToList();
+        foreach(Student student in z)
+        {
+            Console.WriteLine(student.Name);
+        }
+    }
+}
---
+++ Csharp Assignments/CsharpTestAssingnments/StudentApp/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Marks { get; set; }
+
+        public Student(int id, string name, double marks)
+        {
+            Id = id;
+            Name = name;
+            Marks = marks;
+        }
+    }
+}
