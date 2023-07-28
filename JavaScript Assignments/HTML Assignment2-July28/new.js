778cd997ac32d13158eb7315aa093f46ffe9a5c9
muhammedashikCQ <muhammed.ashik@controlqore.com>
2023-07-28 16:40:56 +0530

    js Updated

--- JavaScript Assignments/HTML Assignment2-July28/new.js
+++ JavaScript Assignments/HTML Assignment2-July28/new.js
@@ -1,3 +1,54 @@
-x=[{id:2,name:"Ashik",age:23},{id:3,name:"Eldho",age:24},{id:4,name:"ayad",age:28}]
-var y=x.find(x=>x.id===3)
-console.log(y)
+function MyFunction() {
+  Student=[
+    {
+      "FirstName": "John",
+      "LastName": "Doe",
+      "Age": 20,
+      "Department": "Computer Science"
+    },
+    {
+      "FirstName": "Jane",
+      "LastName": "Smith",
+      "Age": 22,
+      "Department": "Physics"
+    },
+    {
+      "FirstName": "Michael",
+      "LastName": "Johnson",
+      "Age": 21,
+      "Department": "Mathematics"
+    },
+    {
+      "FirstName": "Sarah",
+      "LastName": "Williams",
+      "Age": 19,
+      "Department": "Computer Science"
+    },
+    {
+      "FirstName": "Robert",
+      "LastName": "Brown",
+      "Age": 23,
+      "Department": "Mathematics"
+    },
+    {
+      "FirstName": "Emily",
+      "LastName": "Davis",
+      "Age": 20,
+      "Department": "Computer Science"
+    }
+  ]
+  const computerScienceStudent=Student.filter(x=>x.Department==="Computer Science");
+  console.log(computerScienceStudent);
+  const NameofStudent=Student.filter(x=>x.Age>21).map(x=>x.FirstName);
+  console.log(NameofStudent);
+  const Science =Student.some(x=>x.Department === "Computer Science" && x.FirstName == "Robert");
+  console.log(Science);
+  const StudentAge=Student.some(x=>x.Age>23 && x.Department=="Mathematics");
+  console.log(StudentAge);
+  const AllStudentsAge=Student.every(x=>x.Age>18);
+  console.log(AllStudentsAge);
+  const FirstStudent=Student.find(x=>x.FirstName==="John").Department;
+  console.log(FirstStudent);
+}
+
+      
