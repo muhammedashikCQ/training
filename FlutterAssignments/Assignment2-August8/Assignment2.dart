16f0871f530c38807d53382996a876edbaab2ade
muhammedashikCQ <muhammed.ashik@controlqore.com>
2023-08-08 18:33:13 +0530

    Updated

--- FlutterAssignments/Assignment2-August8/Assignment2.dart
+++ FlutterAssignments/Assignment2-August8/Assignment2.dart
@@ -50,7 +50,7 @@ print(x.map((element)=>(element.first)).join(','));
 //Task 6: List Indexing and Sublist
 
 List<int> intlist3=[];
-intlist3.addAll([1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20]);
+intlist3.addAll([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
 print(intlist3.elementAt(5));
 print(intlist3.sublist(10,16));
 
