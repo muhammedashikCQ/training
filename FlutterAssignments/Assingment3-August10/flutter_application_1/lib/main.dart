e1e6ed19c6fd4ed0dec7d131fbd704dfa533f326
muhammedashikCQ <muhammed.ashik@controlqore.com>
2023-08-11 10:05:25 +0530

    Updated

--- FlutterAssignments/Assingment3-August10/flutter_application_1/lib/main.dart
+++ FlutterAssignments/Assingment3-August10/flutter_application_1/lib/main.dart
@@ -2,22 +2,44 @@ import 'package:flutter/material.dart';
  
 void main() => runApp(const MyApp());
  
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget 
+{
+  const MyApp({super.key});
  
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) 
+  {
+    return MaterialApp
+    (
       title: 'Hello world App',
-      home: Scaffold(
-        appBar: AppBar(title:const Text('Demo App')),
-        body: const Center(child: Text('Hello world.')),
-
-        
-        
-
-        
+      debugShowCheckedModeBanner: false,
+      home: Scaffold
+      (
+        appBar: AppBar
+        (          
+          title:const 
+          Text('Demo App')
         ),
+        body: const Center
+        (
+          child:Column
+          (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:
+            [
+              Text('Hello world.',
+              style: TextStyle
+              (
+                fontWeight: FontWeight.bold,
+                fontSize: 30
+              )),
+              Text("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters "
+              )
+            ]
+          ) 
+      ),
+
+      ),
     );
   }
 }
