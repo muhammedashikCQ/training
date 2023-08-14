6fc5c7829344a88cd07d15719d0d871441c143a9
muhammedashikCQ <muhammed.ashik@controlqore.com>
2023-08-14 09:15:33 +0530

    Updated

--- FlutterAssignments/Assignment4-August11/new_flutter_app/lib/main.dart
+++ FlutterAssignments/Assignment4-August11/new_flutter_app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
   
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        
         useMaterial3: true,
       ),
       home: const HomePage(),
