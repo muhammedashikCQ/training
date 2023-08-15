ff5539ea1bcc02a8bb23849f365f5ba1b9016356
Muhammed Ashik A <muhammedashikiris86@gmail.com>
2023-08-15 11:11:40 +0530

    upadated

--- Csharp Assignments/CsharpAssignment11-August14/EmployeeManagment/Controller/LocationController.cs
+++ Csharp Assignments/CsharpAssignment11-August14/EmployeeManagment/Controller/LocationController.cs
@@ -9,7 +9,7 @@ namespace EmployeeManagement.Controller
     [ApiController]
     public class LocationController : ControllerBase
     {
-       
+
 
         private readonly EmployeeApplicationContext? dbContext;
 
@@ -17,53 +17,52 @@ namespace EmployeeManagement.Controller
         {
             this.dbContext = dbContext;
         }
-       
+
         [HttpPost("AddLocation")]
-    
-        public void post(string Name)
+
+        public void post([FromBody] Location location)
         {
-           Location location = new Location();
-            location.Name = Name;
+          
+            
             dbContext?.Add(location);
             dbContext?.SaveChanges();
         }
         [HttpDelete("Deletelocation")]
 
-        public void delete(int id)
+        public void delete(Location location,int id)
         {
-            Location location=new Location();
+            
             location.Id = id;
             dbContext?.Remove(location);
             dbContext?.SaveChanges();
         }
 
         [HttpGet("GetById")]
-        public IActionResult Get(int id)
+        public IActionResult Get(Location location,int id)
         {
-            Location location = new Location();
+           
             return Ok(dbContext?.Location.Find(id));
-            
+
         }
 
         [HttpGet("GetAll")]
-        public IActionResult Get() 
+        public IActionResult Get(Location location)
         {
-            Location location = new Location();
+
             return Ok(dbContext?.Location);
         }
 
         [HttpPut("UpdateTheDetail")]
 
-        public void put(Location location,int id,string name)
+        public void put([FromBody]Location location)
         {
-            var x = dbContext?.Location.Find(id);
-            x.Name = name;
+            
             dbContext?.SaveChanges();
         }
 
 
-        
-        
+
+
 
 
 
